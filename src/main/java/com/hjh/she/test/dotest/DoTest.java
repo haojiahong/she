b103e9b4d12380b03b09f32e4c17b76027e76293
Jokerone <1548620258@qq.com>
2015-11-02 13:31:55 +0800

    修改了测试类

--- src/main/java/com/hjh/she/test/dotest/DoTest.java
+++ src/main/java/com/hjh/she/test/dotest/DoTest.java
@@ -13,7 +13,7 @@ import com.hjh.she.test.service.UserServcie;
  * @desc 测试方法没有写在src/test/java文件夹下，感觉这样maven，install的时候总是要在加载一遍麻烦，
  *       这样会造成打包的时候把test类也打包到正式项目中，所以打包的时候，可以先删除。
  */
-public class TestSpring {
+public class DoTest {
 	@Test
 	public void test() {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
